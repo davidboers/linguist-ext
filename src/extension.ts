4d460d865454744dff52c25e75020436857ab5aa
davidboers <kwmmineccraft@gmail.com>
2024-12-27 14:18:58 -0500

    Preliminary work

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,42 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as ChildProcess from 'child_process';
+import { findRubyExec, findGemExec, isGemInstalled } from './ruby';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	vscode.workspace.onDidOpenTextDocument(async (document: vscode.TextDocument) => await activeServer(context, document));
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "linguist" is now active!');
+	let ruby = findRubyExec();
+	let gem = findGemExec();
+	if (!gem && !isGemInstalled(gem)) {
+		vscode.window.showErrorMessage('Gem not installed. Install using `gem install github-linguist`');
+	}
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('linguist.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from github-linguist!');
+	console.log('Linguist extension is now active.');
+
+	const inquireFile = vscode.commands.registerCommand('linguist.inquireFile', () => {
+		const editor = vscode.window.activeTextEditor;
+		if (!editor) {
+			vscode.window.showInformationMessage('No active document.');
+			return;
+		}
+
+		const path = editor.document.uri.path;
+		const out = ChildProcess.spawnSync('github-linguist', [path]);
+		if (out.status !== 0) {
+			vscode.window.showErrorMessage(`Something went wrong. Linguist gem returned error code: ${out.status}`);
+			return;
+		}
 	});
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(inquireFile);
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+function activeServer(context: vscode.ExtensionContext, document: vscode.TextDocument) {
+	const uri = document.uri;
+	console.log('File opened.');
+}
+
+
+export function deactivate() {
+	console.log('Linguist extension is no longer active.');
+}
---
+++ src/ruby.ts
@@ -0,0 +1,38 @@
+import * as ChildProcess from 'child_process';
+import { workspace } from 'vscode';
+
+function findExecTemp(setting: string, execName: string): string {
+    let exePath = workspace.getConfiguration('linguist').get(setting) as string;
+    if (executableExists(exePath)) {
+        return exePath;
+    } else {
+        const msg = `Could not find ${execName} executable at '${exePath}'! 
+        Consider changing the 'linguist.${setting}' setting, and make sure the executable is installed on your computer by running '${findCommand()} ${execName.toLowerCase()}'`;
+        throw new Error(msg);
+    }
+}
+
+export function findRubyExec(): string {
+    return findExecTemp('rubyExecutable', 'Ruby');
+}
+
+export function findGemExec(): string {
+    return findExecTemp('gemExecutable', 'Gem');
+}
+
+export function isGemInstalled(gemExec: string): boolean {
+    const args = ['list', '-i', 'github-linguist'];
+    const out = ChildProcess.spawnSync(gemExec, args);
+    return out.status === 0;
+}
+
+function findCommand(): string {
+    const isWindows = process.platform === 'win32';
+    return isWindows ? 'where' : 'which';
+}
+
+function executableExists(exe: string): boolean {
+    const cmd: string = findCommand();
+    const out = ChildProcess.spawnSync(cmd, [exe]);
+    return out.status === 0;
+}
