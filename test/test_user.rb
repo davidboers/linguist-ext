99b2c881dd0b81062e11a0277bf9dfb4bdb3364f
davidboers <kwmmineccraft@gmail.com>
2024-12-27 22:51:47 -0500

    multiple repos

---
+++ test/test_user.rb
@@ -0,0 +1,19 @@
+require_relative './helper'
+require_relative './myrepos'
+
+class TestUser < Minitest::Test
+    include Linguist
+
+    def test_multiple_repos
+        repos = Linguist.myrepos
+        summaries = []
+        repos.each do |repo|
+            puts repo
+            repo = index_repo(repo)
+            s = Summary.new repo
+            summaries.push(s)
+        end
+        super_tally = merge_summaries(summaries)
+        super_tally.present
+    end
+end
