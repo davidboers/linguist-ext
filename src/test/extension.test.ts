99ac731da4aa7f7870d0d117ae30952a61ec4ba5
davidboers <kwmmineccraft@gmail.com>
2024-12-27 19:36:27 -0500

    test suite

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -1,15 +1,18 @@
 import * as assert from 'assert';
 
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
 import * as vscode from 'vscode';
 // import * as myExtension from '../../extension';
 
 suite('Extension Test Suite', () => {
 	vscode.window.showInformationMessage('Start all tests.');
+	const extensionId = 'undefined_publisher.linguist';
+	const extension = vscode.extensions.getExtension(extensionId);
 
-	test('Sample test', () => {
-		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
-		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
+	test('1. Activation', async () => {
+		assert.ok(await extension?.activate().then(() => true));
+	});
+
+	test('2. Inquire file command.', async () => {
+		assert.ok(await vscode.commands.executeCommand('linguist.inquireFile').then(() => true));
 	});
 });
