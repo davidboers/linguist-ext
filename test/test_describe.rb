a2ccd11d70f34f863725c22da8bf0a9d794e164f
davidboers <kwmmineccraft@gmail.com>
2024-12-29 18:17:50 -0500

    describe test

---
+++ test/test_describe.rb
@@ -0,0 +1,23 @@
+require_relative './helper'
+
+class TestDescribe < Minitest::Test
+  include Linguist
+
+  def test_org
+    s = index_org('NewElectoralCollege')
+    org = Org.new(s)
+    File.write('test/descriptions/org.html', org.describe)
+  end
+
+  def test_repo
+    s = index_repo('.')
+    repo = Repo.new(s)
+    File.write('test/descriptions/repo.html', repo.describe)
+  end
+
+  def test_user
+    s = index_user('davidboers')
+    user = User.new(s)
+    File.write('test/descriptions/user.html', user.describe)
+  end
+end
