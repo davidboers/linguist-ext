b85250a995073b885e2b6827733dac5b38f7310e
davidboers <kwmmineccraft@gmail.com>
2024-12-30 00:26:12 -0500

    Abstractified

--- lib/linguist-ext/describe.rb
+++ lib/linguist-ext/describe.rb
@@ -3,14 +3,61 @@ require 'erb'
 module Linguist
   class Describable
     include Helper
-    
-    attr_reader :summary
 
-    def initialize(summary)
+    attr_reader :summary
+    attr_reader :main_lang
+
+    def initialize(summary, noun)
       @summary = summary
+      @noun = noun
+      @summary.tallies.sort_by! { |t| -t.bytes }
+      unless summary.tallies.empty?
+        @main_lang = summary.tallies.first
+      end
     end
 
-    def describe(template_path)
+    def byte_share(bytes)
+      (bytes / summary.totalbytes.to_f * 100).round 2
+    end
+
+    def main_msg
+      case summary.tallies.length
+      when 0
+        no_langs
+      when 1
+        one_lang
+      else
+        multiple_langs
+      end
+    end
+
+    def make_title
+      raise NotImplementedError
+    end
+
+    def maj(nm)
+      largest_shr = byte_share(summary.tallies.first.bytes)
+      if largest_shr >= 50 && largest_shr != 100
+        "Most (#{largest_shr}%) of this #{nm}'s code is written in #{main_lang.language}."
+      else
+        ''
+      end
+    end
+
+    def no_langs
+      raise NotImplementedError
+    end
+
+    def one_lang
+      raise NotImplementedError
+    end
+
+    def multiple_langs(msg)
+      raise NotImplementedError
+    end
+
+    def describe
+      template_path = 'lib/linguist-ext/templates/description.html.erb'
       template = File.read(template_path)
       renderer = ERB.new(template)
       renderer.result(binding)
@@ -18,20 +65,71 @@ module Linguist
   end
 
   class User < Describable
-    def describe(template_path = 'lib/linguist-ext/templates/user.html.erb')
-      super(template_path)
+    def initialize(summary)
+      super(summary, 'user')
+    end
+
+    def make_title
+      'User breakdown'
+    end
+
+    def no_langs
+      'There are no programming languages in this user\'s repositories.'
+    end
+
+    def one_lang
+      "This user writes in #{main_lang.name}."
+    end
+
+    def multiple_langs
+      langs = merge_langs(summary.tallies[0..2].map(&:language))
+      "This user\'s preferred languages are #{langs}."
     end
   end
 
   class Org < Describable
-    def describe(template_path = 'lib/linguist-ext/templates/org.html.erb')
-      super(template_path)
+    def initialize(summary)
+      super(summary, 'organization')
+    end
+
+    def make_title
+      'Organization breakdown'
+    end
+
+    def no_langs
+      'There are no programming languages in this organization\'s repositories.'
+    end
+
+    def one_lang
+      "This organization writes in #{main_lang.name}."
+    end
+
+    def multiple_langs
+      langs = merge_langs(summary.tallies[0..2].map(&:language))
+      "This organization\'s preferred languages are #{langs}."
     end
   end
 
   class Repo < Describable
-    def describe(template_path = 'lib/linguist-ext/templates/repo.html.erb')
-      super(template_path)
+    def initialize(summary)
+      super(summary, 'repository')
+    end
+
+    def make_title
+      'Repository breakdown'
+    end
+
+    def no_langs
+      'There are no programming languages in this repository.'
+    end
+
+    def one_lang
+      "This repository is written in #{main_lang.name}."
+    end
+
+    def multiple_langs
+      big_langs = summary.big_languages
+      "This repository is written in #{merge_langs(big_langs)}."
     end
   end
 end
