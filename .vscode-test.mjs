24970c2167358a325de36a39bf7718db4db58ec4
davidboers <kwmmineccraft@gmail.com>
2024-12-30 13:31:42 -0500

    Extend timeout to 60 seconds

--- .vscode-test.mjs
+++ .vscode-test.mjs
@@ -2,5 +2,8 @@ import { defineConfig } from '@vscode/test-cli';
 
 export default defineConfig({
 	files: 'out/test/**/*.test.js',
-	workspaceFolder: './test'
+	workspaceFolder: './test',
+	mocha: {
+		timeout: 60 * 1000 // 1 minute
+	}
 });
